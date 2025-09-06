c84a025a12396bbb74449e57768b4bcada7e754f
robie <robie192002@gmail.com>
2025-09-06 22:16:39 +0800

    setup backend

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
